a05decbf3908e002c475c25c02fa2b1e4e96ce10
CSN123 <Corysn123@live.com>
2016-03-06 23:28:12 -0800

    adds first file

--- 01_First/main.go
+++ 01_First/main.go
@@ -1,1 +1,35 @@
-package _1_First
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+type person struct {
+	Name  string
+	Major string
+}
+
+type student struct {
+	person
+	Graduate bool
+}
+
+func main() {
+	p1 := student{
+		person: person{
+			Name:  "Cory Nelson",
+			Major: "Computer Science",
+		},
+		Graduate: false,
+	}
+	tpl, err := template.ParseFiles("conditional.gohtml")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.Execute(os.Stdout, p1)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
